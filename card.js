d99f0f349cacd0db04f21fccb7cc6f70cddd5047
RahulPB08 <r.p.bhosle08@gmail.com>
2024-01-12 10:17:34 +0530

    comment

--- card.js
+++ card.js
@@ -6,9 +6,9 @@ let img = document.querySelector(".img");
 // console.log(img);
 scrolll.onclick = () => {
     img.scrollBy(-1535,0);
-    console.log(-1000);
+    // console.log(-1000);
 };
 scrollr.onclick = () => {
     img.scrollBy(1535,0);
-    console.log(1000);
+    // console.log(1000);g
 };
